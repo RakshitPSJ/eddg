6b42d2561d408aadd015873405829c7f3843eca5
RakshitPSJ <90741291+RakshitPSJ@users.noreply.github.com>
2021-09-17 15:45:48 +0530

    Update EDDController.java

--- src/main/java/com/hbc/eddg/controller/EDDController.java
+++ src/main/java/com/hbc/eddg/controller/EDDController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1")
 public class EDDController {
-    @GetMapping("/")
+    @GetMapping("")
     public ResponseEntity<String> getEdd() {
         return ResponseEntity.ok().body("Hello world");
     }
