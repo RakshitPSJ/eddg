cc6820d420a2c5b1d3203a73d9416c17ae4e8976
Rakshit Jain <rakjain1@publicisgroupe.net>
2021-09-17 11:04:03 +0530

    first commit

---
+++ src/main/java/com/hbc/eddg/EddgApplication.java
@@ -0,0 +1,13 @@
+package com.hbc.eddg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EddgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EddgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hbc/eddg/EddgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hbc.eddg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EddgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
